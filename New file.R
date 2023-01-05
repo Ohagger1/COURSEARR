06c2820e440a957304842d1e50c30943240289bb
Ohagger1 <95619732+Ohagger1@users.noreply.github.com>
2023-01-05 09:28:08 +0000

    new R graph and png

--- New file.R
+++ New file.R
@@ -1,1 +1,10 @@
-print("Hello")
+install.packages('palmerpenguins')
+library('palmerpenguins')
+
+library('ggplot2')
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(colour=species))
+
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(colour=species, shape=species))
+
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(colour=species, shape=species))+facet_wrap(~species)
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(colour=species, shape=species))+facet_wrap(~species)+labs(title="Palmer penguins")
