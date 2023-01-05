04685744845e759f0ebeef3ca72fb5762bb0d873
Ohagger1 <95619732+Ohagger1@users.noreply.github.com>
2023-01-05 11:23:28 +0000

    Create Tooth growth.R

---
+++ Tooth growth.R
@@ -0,0 +1,19 @@
+data("ToothGrowth")
+view(ToothGrowth)
+
+install.packages('dply2')
+library(dplyr)
+filtered_tooth <-  filter(ToothGrowth,dose ==0.5)
+View(filtered_tooth)
+
+
+arrange(filtered_tooth,len)
+
+#Pipe operators
+filtered_toothgrowth <- ToothGrowth %>%
+  filter(dose>0.5) %>% 
+  group_by(supp) %>%
+  summarize(mean_len = mean(len,na.rm = T),.group="drop")
+
+#view function
+filtered_toothgrowth
