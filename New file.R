cb74eaaf4817ceb345314ce713ca596410d83f50
Ohagger1 <95619732+Ohagger1@users.noreply.github.com>
2023-01-05 10:31:18 +0000

    new changes to Rcoursea

--- New file.R
+++ New file.R
@@ -4,7 +4,13 @@ library('palmerpenguins')
 library('ggplot2')
 ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(colour=species))
 
+#Changed shape and colour
 ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(colour=species, shape=species))
 
+#Facet wrap added to seperate the grpahs
 ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(colour=species, shape=species))+facet_wrap(~species)
-ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(colour=species, shape=species))+facet_wrap(~species)+labs(title="Palmer penguins")
+
+#Title added
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(colour=species, shape=species))+
+  facet_wrap(~species)+labs(title="Palmer penguins")
+
